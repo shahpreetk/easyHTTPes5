9c4451b9de400f9ae5f654ea7c6d741453933fe9
Preet Shah <preetshah@Preets-MacBook-Pro.local>
2019-08-16 11:14:57 +0530

    post method

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const http = new easyHTTP;
 
 // GET POSTS
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+// });
+
+// GET SINGLE POST
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// CREATE DATA
+const data={
+    title:'Custom Post',
+    body:'this is a custom post'
+};
+
+// CREATE POST
+http.post('https://jsonplaceholder.typicode.com/posts',data,function(err,post){
     if (err) {
-        console.log(err);
-    } else {
-        console.log(posts);
-    }
-});
+                console.log(err);
+            } else {
+                console.log(post);
+            }
+})
--- easyhttp.js
+++ easyhttp.js
@@ -3,15 +3,15 @@ function easyHTTP() {
 }
 
 // MAKE AN HTTP GET REQUEST
-easyHTTP.prototype.get = function (url,callback) {
+easyHTTP.prototype.get = function (url, callback) {
     this.http.open('GET', url, true);
 
     let self = this;
     this.http.onload = function () {
         if (self.http.status === 200) {
-            callback(null,self.http.responseText);
-        }else{
-            callback('Error: '+self.http.status);
+            callback(null, self.http.responseText);
+        } else {
+            callback('Error: ' + self.http.status);
         }
     }
 
@@ -19,7 +19,17 @@ easyHTTP.prototype.get = function (url,callback) {
 }
 
 // MAKE AN HTTP POST REQUEST
+easyHTTP.prototype.post = function (url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
 
+    let self = this;
+    this.http.onload = function () {
+        callback(null, self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
 // MAKE AN HTTP PUT REQUEST
 
 // MAKE AN HTTP DELETE REQUEST

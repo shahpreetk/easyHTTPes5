06b823ce1ac0d3cc4cab438f99846756a4a061c7
Preet Shah <preetshah@Preets-MacBook-Pro.local>
2019-08-16 10:47:38 +0530

    3rd commit

---
+++ easyhttp.js
@@ -0,0 +1,23 @@
+function easyHTTP() {
+    this.http = new XMLHttpRequest();
+}
+
+// MAKE AN HTTP GET REQUEST
+easyHTTP.prototype.get = function (url) {
+    this.http.open('GET', url, true);
+
+    let self = this;
+    this.http.onload = function () {
+        if (self.http.status === 200) {
+            console.log(this.http.responseText);
+        }
+    }
+
+    this.http.send();
+}
+
+// MAKE AN HTTP POST REQUEST
+
+// MAKE AN HTTP PUT REQUEST
+
+// MAKE AN HTTP DELETE REQUEST
